26507c654c96970439449274801154e9a5107f7d
aisaishahptri-dev <aisaishahptri@gmail.com>
2026-05-17 09:51:47 +0700

    Database Migration - table

---
+++ database/migrations/2026_05_16_011546_create_izin_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('izin', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('siswa_id')->constrained('siswa')->onDelete('cascade');
+            $table->foreignId('disetujui_oleh')->constrained('users')->onDelete('cascade');
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->string('alasan');
+            $table->enum('status', ['pending', 'disetujui', 'ditolak'])->default('pending');
+            $table->string('lampiran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('izin');
+    }
+};
