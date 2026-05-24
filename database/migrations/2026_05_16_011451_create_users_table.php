741f5a14093990c51c883a0c51deb2962aaf6ec8
aisaishahptri-dev <aisaishahptri@gmail.com>
2026-05-24 11:22:43 +0700

    update auth

--- database/migrations/2026_05_16_011451_create_users_table.php
+++ database/migrations/2026_05_16_011451_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', ['admin', 'guru', 'siswa']);
+            $table->enum('role', ['admin', 'guru', 'siswa'])->default('siswa');
             $table->timestamps();});
     }
 
